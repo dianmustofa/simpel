dcde32359b1662955a75464274938aa980b53018
dianmustofa <mustofa.d@outlook.com>
2024-10-30 12:54:39 +0700

    edit map

--- application/views/Isu/isu_edit_view.php
+++ application/views/Isu/isu_edit_view.php
@@ -62,7 +62,7 @@
                     <!-- Basic Horizontal form layout section start -->
                     <section id="basic-horizontal-layouts">
                         <div class="row match-height">
-                            <div class="col-md-6 col-12">
+                            <div class="col-md-12 col-12">
                                 <div class="card">
                                     <div class="card-header">
                                         <h4 class="card-title">Edit Isu Lingkungan</h4>
@@ -309,19 +309,113 @@
                                                                 name="alamat_isu" value="<?php echo isset($edit_isu['alamat_isu']) ? $edit_isu['alamat_isu'] : ''; ?>">
                                                         </div>
                                                         <div class="col-md-4">
+                                                            <label>Peta Lokasi</label>
+                                                        </div>
+                                                        <div class="col-md-8 form-group">
+                                                            <div>
+                                                                <style>
+                                                                    #map {
+                                                                        height: 400px;
+                                                                    }
+                                                                </style>
+                                                                <div id="map"></div>
+
+                                                                <script>
+                                                                    var map = L.map('map').setView([<?php echo $edit_isu['latitude'];?>, <?php echo $edit_isu['longitude'];?>], 15);
+
+                                                                    L.tileLayer('http://{s}.google.com/vt/lyrs=y&x={x}&y={y}&z={z}', {
+                                                                        maxZoom: 20,
+                                                                        subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+                                                                    }).addTo(map);
+
+                                                                    L.Control.geocoder({
+                                                                        defaultMarkGeocode: false
+                                                                    }).on('markgeocode', function(e) {
+                                                                        var latlng = e.geocode.center;
+                                                                        L.marker(latlng).addTo(map)
+                                                                            .bindPopup(e.geocode.name)
+                                                                            .openPopup();
+                                                                        map.setView(latlng, 16);
+                                                                    }).addTo(map);
+
+                                                                    // Menambahkan marker awal
+                                                                    var initialMarker = L.marker([<?php echo $edit_isu['latitude'];?>, <?php echo $edit_isu['longitude'];?>]).addTo(map)
+                                                                        .bindPopup('<?php echo $edit_isu['title_isu'];?>')
+                                                                        .openPopup();
+
+                                                                    var marker = null;
+                                                                    // Event listener untuk menangkap klik pada peta
+                                                                    map.on('click', function(e) {
+                                                                        var lat = e.latlng.lat;
+                                                                        var lng = e.latlng.lng;
+
+                                                                        document.getElementById('latitudeInput').value = lat.toFixed(5);
+                                                                        document.getElementById('longitudeInput').value = lng.toFixed(5);
+
+                                                                        // // Perbarui nilai latitude dan longitude pada label
+                                                                        // document.getElementById('latitudeValue').innerText = lat.toFixed(5);
+                                                                        // document.getElementById('longitudeValue').innerText = lng.toFixed(5);
+
+                                                                        // Jika marker sudah ada, hapus marker sebelumnya
+                                                                        if (marker) {
+                                                                            map.removeLayer(marker);
+                                                                        }
+
+                                                                        // Tambahkan marker baru pada posisi yang diklik
+                                                                        marker = L.marker([lat, lng]).addTo(map)
+                                                                            .bindPopup('Marker baru: ' + lat.toFixed(5) + ', ' + lng.toFixed(5))
+                                                                            .openPopup();
+
+                                                                        // // Tambahkan marker baru pada posisi yang diklik
+                                                                        // marker = L.marker([lat, lng]).addTo(map);
+                    
+                                                                    });
+
+
+                                                                    // var marker = L.marker([<?php echo $edit_isu['latitude'];?>, <?php echo $edit_isu['longitude'];?>]).addTo(map)
+                                                                    //     .bindPopup('<?php echo $edit_isu['title_isu'];?>')
+                                                                    //     .openPopup();
+
+                                                                    
+                                                                </script>
+                                                            </div>
+                                                        </div>
+
+                                                        <!-- <label for="latitude">Latitude : <span id="latitudeValue"></span></label>
+                                                        <input type="hidden" name="latitude" id="latitudeInput" required readonly><br>
+                                                        <label for="longitude">Longitude : <span id="longitudeValue"></span></label>
+                                                        <input type="hidden" name="longitude" id="longitudeInput" required readonly><br> -->
+
+                                                        <!-- <div class="col-md-4">
                                                             <label>Latitude</label>
                                                         </div>
                                                         <div class="col-md-8 form-group">
                                                             <input type="text" class="form-control"
-                                                                name="latitude" value="<?php echo isset($edit_isu['latitude']) ? $edit_isu['latitude'] : ''; ?>">
+                                                                name="latitude" value="<?php echo isset($edit_isu['latitude']) ? $edit_isu['latitude'] : ''; ?>" readonly>
                                                         </div>
                                                         <div class="col-md-4">
                                                             <label>Longitude</label>
                                                         </div>
                                                         <div class="col-md-8 form-group">
                                                             <input type="text" class="form-control"
-                                                                name="longitude" value="<?php echo isset($edit_isu['longitude']) ? $edit_isu['longitude'] : ''; ?>">
+                                                                name="longitude" value="<?php echo isset($edit_isu['longitude']) ? $edit_isu['longitude'] : ''; ?>" readonly>
+                                                        </div> -->
+
+                                                        <div class="col-md-4">
+                                                            <label for="latitude">Latitude</label>
                                                         </div>
+                                                        <div class="col-md-8 form-group">
+                                                            <input type="text" class="form-control"
+                                                                name="latitude" id="latitudeInput" value="<?php echo isset($edit_isu['latitude']) ? $edit_isu['latitude'] : ''; ?>" readonly>
+                                                        </div>
+                                                        <div class="col-md-4">
+                                                            <label for="longitude">Longitude</label>
+                                                        </div>
+                                                        <div class="col-md-8 form-group">
+                                                            <input type="text" class="form-control"
+                                                                name="longitude" id="longitudeInput" value="<?php echo isset($edit_isu['longitude']) ? $edit_isu['longitude'] : ''; ?>" readonly>
+                                                        </div>
+                                                        
                                                         <div class="col-md-4">
                                                             <label>Tanggal Upload</label>
                                                         </div>
@@ -329,16 +423,7 @@
                                                             <input type="text" class="form-control"
                                                                 name="last_created_date" value="<?php echo isset($edit_isu['last_created_date']) ? $edit_isu['last_created_date'] : ''; ?>" readonly>
                                                         </div>
-                                                        <!-- <div class="col-md-4">
-                                                            <label>Dokumentasi</label>
-                                                        </div>
-                                                        <div class="col-md-8 form-group">
-                                                            <input type="text" class="form-control"
-                                                                name="fname" value="You can't update me :P">
-                                                        </div> -->
-
-                                                        
-
+                                                     
                                                         <!-- Input Isu -->
                                                         <div class="col-md-4">
                                                             <label>Instansi Tujuan</label>
@@ -357,10 +442,17 @@
                                                                 name="status_usulan" value="<?php echo isset($edit_isu['status_usulan']) ? $edit_isu['status_usulan'] : ''; ?>" readonly>
                                                         </div>
 
-                                                        <div class="col-md-12 form-group">
+                                                        <div class="col-md-4">
+                                                            <label>Komentar</label>
+                                                        </div>
+                                                        <div class="col-md-8 form-group">
+                                                        <textarea type="text" class="form-control" name="komentar_usulan" id="exampleFormControlTextarea1" rows="3" readonly><?php echo isset($edit_isu['komentar_usulan']) ? $edit_isu['komentar_usulan'] : ''; ?></textarea>
+                                                        </div>
+
+                                                        <!-- <div class="col-md-12 form-group">
                                                             <label for="exampleFormControlTextarea1" class="form-label">Komentar</label>
                                                             <textarea type="text" class="form-control" name="komentar_usulan" id="exampleFormControlTextarea1" rows="3" readonly><?php echo isset($edit_isu['komentar_usulan']) ? $edit_isu['komentar_usulan'] : ''; ?></textarea>
-                                                        </div>
+                                                        </div> -->
 
                                                         <div class="col-md-4">
                                                             <label>Upload Gambar</label>
@@ -398,7 +490,7 @@
                                 </div>
                             </div>
                             
-                            <div class="col-md-6 col-12">
+                            <!-- <div class="col-md-6 col-12">
                                 <div class="card">
                                     <div class="card-header">
                                         <h4 class="card-title">Peta Lokasi</h4>
@@ -409,22 +501,13 @@
                                                 <div class="form-body">
                                                     <div id="map"></div>
 
-                                                    <!-- Tambahkan CSS untuk ukuran peta -->
                                                     <style>
                                                         #map {
-                                                            height: 400px; /* Atur tinggi peta */
+                                                            height: 400px;
                                                         }
                                                     </style>
                                                     <script>
                                                         var map = L.map('map').setView([<?php echo $edit_isu['latitude'];?>, <?php echo $edit_isu['longitude'];?>], 15);
-
-                                                        // L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                                                        //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                                                        // }).addTo(map);
-                                                        // L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
-                                                        //         maxZoom: 20,
-                                                        //         subdomains:['mt0','mt1','mt2','mt3']
-                                                        // }).addTo(map);
 
                                                         L.tileLayer('http://{s}.google.com/vt/lyrs=y&x={x}&y={y}&z={z}', {
                                                             maxZoom: 20,
@@ -438,7 +521,7 @@
                                                             L.marker(latlng).addTo(map1)
                                                                 .bindPopup(e.geocode.name)
                                                                 .openPopup();
-                                                            map.setView(latlng, 16); // Zoom in ketika lokasi ditemukan
+                                                            map.setView(latlng, 16);
                                                         }).addTo(map);
 
 
@@ -453,7 +536,8 @@
                                         </div>
                                     </div>
                                 </div>
-                            </div>
+                            </div> -->
+
                         </div>
                     </section>
                     <!-- // Basic Horizontal form layout section end -->
